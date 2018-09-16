d2e8f3d0d601d7fe9c9e601955a3e273e4e81e7e
ssydz <925584224@qq.com>
2018-09-16 10:38:44 +0800

    feat:使用jsx

--- src/js/components/Body.js
+++ src/js/components/Body.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
-class Body extends React.Component{
-    render(){
+class Body extends React.Component {
+    render() {
+        let userName = 'Sunny';
+        let able = false;
+
+        let html1 = "practice\u0020jsx";
+        let html2 = "practice&nbsp;jsx";
         return (
             <div>
                 <h2>页面主体内容</h2>
+                {/*三元表达式*/}
+                <p>{userName === '' ? '用户没有登录' : '用户名：' + userName}</p>
+                {/*动态绑定属性值*/}
+                <input type="text" disabled={able}/>
+
+                {/*需要进行Unicode转码*/}
+                <p>{html1}</p>
+
+                {/*无需进行转码，但是操作不安全*/}
+                <p dangerouslySetInnerHTML={{__html: html1}}></p>
             </div>
         )
     }
